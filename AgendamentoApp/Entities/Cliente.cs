d882ae1e974d74e0dc7f02386c30c8f6cafdc008
Manoela Moreno Frattini <84809830+manoela27@users.noreply.github.com>
2025-05-26 23:51:41 -0300

    Add AgendamentoApp/Entities/Cliente.cs 1748314313547

---
+++ AgendamentoApp/Entities/Cliente.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendamentoApp.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
