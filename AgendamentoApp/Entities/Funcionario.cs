1b9bf16bb290cd184ac7e68247958e06bf55d16a
Manoela Moreno Frattini <84809830+manoela27@users.noreply.github.com>
2025-05-26 23:51:40 -0300

    Add AgendamentoApp/Entities/Funcionario.cs 1748314312296

---
+++ AgendamentoApp/Entities/Funcionario.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendamentoApp.Entities
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        public string Senha { get; set; }
+
+        public virtual ICollection<Servico> Servicos { get; set; }
+
+        public Funcionario()
+        {
+            Servicos = new HashSet<Servico>();
+        }
+    }
+}
